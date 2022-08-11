dd66eaee5e7b9c85e9c4eb326d6b6a60e365d6c0
Fernando Paulino <xizes2@hotmail.com>
2022-08-11 19:31:31 +0200

    create Procfile with config to Heroku

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,24 @@ import Debug from "debug";
 import express from "express";
 import chalk from "chalk";
 
-const debug = Debug("api-rest-backend:index.ts");
+const debug = Debug("api-rest-backend:index.ts"); // esto para que debuguee solo en los modulos de mi archivo index
 const app = express();
 
 app.use(express.json());
 const port = process.env.PORT ?? 4000;
 
-const thingsIknow = ["redux", "html", "css"];
+const thingsIknow = [
+  { id: 1, name: "redux" },
+  { id: 2, name: "html" },
+  { id: 3, name: "css" },
+];
 
-app.get("/data", (req, res) => {
+app.get("/thingsIKnow", (req, res) => {
   debug(chalk.green(`A request has arrived on port ${req.url}`));
-  res.status(200).json({ thingsIknow });
+  res.status(200).json(thingsIknow);
 });
+
+app.get("/thingsIKnow/");
 
 app.listen(port, () => {
   debug(chalk.yellowBright(`Server listening on port ${port}`));
